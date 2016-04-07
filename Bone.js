1650a2e984e0db1d0ad53a9e626d4a07b2d082ac
Mary <fznamznon23@gmail.com>
2016-04-07 20:47:39 +0300

    bones have a lot motions

--- Bone.js
+++ Bone.js
@@ -1,9 +1,9 @@
 var Bones = [];
 
-function Bone(filename, VertexPositionBuffer, VertexIndicesBuffer, Joint, Visible, parent, childs) {
+function Bone(filename, Joint, Visible, parent, childs) {
     this.filename = filename;
-    this.VertexPositionBuffer = VertexPositionBuffer;
-    this.VertexIndicesBuffer = VertexIndicesBuffer;
+    this.VertexPositionBuffer = null;
+    this.VertexIndicesBuffer = null;
     this.Joint = Joint;
     this.Visible = Visible;
     this.Childs = childs;
@@ -12,12 +12,15 @@ function Bone(filename, VertexPositionBuffer, VertexIndicesBuffer, Joint, Visibl
         parent.Childs.push(this);
 }
 
-function Joint(location, orientation, movable, motion) {
+function Joint(location, orientation, movable) {
 
     this.location = location;
     this.orientation = orientation;
     this.movable = movable;
-    this.Motion = motion;
+    this.Motions = [];
+    this.addMotion = function (motion) {
+        this.Motions.push(motion);
+    }
 }
 
 function Motion(axis, min, max, cur_value) {
@@ -25,11 +28,12 @@ function Motion(axis, min, max, cur_value) {
     this.min = min;
     this.max = max;
     this.cur_value = cur_value;
-    
 }
 
 
-var humerus = new Bone("bones/humerus.asc", null, null, null, true, null, [] );
+
+
+var humerus = new Bone("bones/humerus.asc", null, true, null, [] );
 
 var elbow_flexion = new Motion([0.226046935824, 0.191039945763, 0.955199728815],
     0.0,
@@ -37,14 +41,16 @@ var elbow_flexion = new Motion([0.226046935824, 0.191039945763, 0.955199728815],
     0.0
 );
 
+
 var elbow = new Joint(
     [0.0127, -0.274, 0.0389],
     [0, 0, 0],
-    true,
-    elbow_flexion
+    true
 );
 
-var ulna = new Bone("bones/_ulna.asc", null, null, elbow, true, humerus, [] );
+elbow.addMotion(elbow_flexion);
+
+var ulna = new Bone("bones/_ulna.asc", elbow, true, humerus, [] );
 
 var pro_sup = new Motion(
     [0.056398022307, 0.983577389037, -0.171449067814],
@@ -58,20 +64,21 @@ var radioulnar = new Joint(
 
     [-0.0065, -0.008, 0.027],
     [0, 0, 0],
-    true,
-    pro_sup
+    true
 );
 
-var radius = new Bone("bones/_radius.asc", null, null, radioulnar, true, ulna, []);
+radioulnar.addMotion(pro_sup);
+
+var radius = new Bone("bones/_radius.asc", radioulnar, true, ulna, []);
 
 var rad_rotaxis = new Joint(
     [-0.0085, -0.2313, 0.0561],
     [-0.261799387799, 0.392699081699, 0.0],
-    false,
-    null
+    false
+
 );
 
-var rotaxis = new Bone(null, null, null, rad_rotaxis, false, radius, []);
+var rotaxis = new Bone(null, rad_rotaxis, false, radius, []);
 
 var flexion = new Motion(
     [0, 0, 1],
@@ -83,10 +90,12 @@ var flexion = new Motion(
 var rotaxis_auxprfem = new Joint(
     [-0.001, 0.009, 0.0],
     [0, 0, 0],
-    true,
-    flexion);
+    true
+);
 
-var auxprfem = new Bone(null, null, null, rotaxis_auxprfem, false, rotaxis, []);
+rotaxis_auxprfem.addMotion(flexion);
+
+var auxprfem = new Bone(null, rotaxis_auxprfem, false, rotaxis, []);
 
 var deviation = new Motion(
     [1, 0, 0],
@@ -98,134 +107,180 @@ var deviation = new Motion(
 var auxprfem_auxprrud = new Joint(
     [0, 0, 0],
     [0, 0, 0],
-    true,
-    deviation
+    true
 );
+auxprfem_auxprrud.addMotion(deviation);
 
-var auxprrud = new Bone(null, null, null, auxprfem_auxprrud, false, auxprfem, []);
+
+var auxprrud = new Bone(null, auxprfem_auxprrud, false, auxprfem, []);
 
 var auxprrud_auxdrfem = new Joint(
     [0.001, -0.02, 0.0],
     [0, 0, 0],
-    false,
-    null
+    false
 );
 
-var auxdrfem = new Bone(null, null, null, auxprrud_auxdrfem, false, auxprrud, []);
+var auxdrfem = new Bone(null, auxprrud_auxdrfem, false, auxprrud, []);
 
 var auxdrfem_auxdrrud = new Joint(
     [0, 0, 0],
     [0, 0, 0],
-    false,
-    null
+    false
 );
 
-var auxdrrud = new Bone(null, null, null, auxdrfem_auxdrrud, false, auxdrfem, []);
+
+var auxdrrud = new Bone(null, auxdrfem_auxdrrud, false, auxdrfem, []);
 
 var prox_scaph = new Joint(
     [-0.0035, -0.0025, 0.009],
     [0.401425727959, 0.0, 0.0],
-    false,
-    null
+    false
 );
 
-var scaphoid = new Bone("bones/_r_scaph.asc", null, null, prox_scaph, true, auxprrud, []);
+var scaphoid = new Bone("bones/_r_scaph.asc", prox_scaph, true, auxprrud, []);
 
 var prox_lun = new Joint(
     [-0.004, -0.0015, -0.007],
     [0, 0, 0],
-    false,
-    null
+    false
 );
 
-var lunate = new Bone("bones/_r_lun.asc", null, null, prox_lun, true, auxprrud, []);
+var lunate = new Bone("bones/_r_lun.asc", prox_lun, true, auxprrud, []);
 
 var dist_trpzm = new Joint(
     [0.0035, -0.0035, 0.02],
     [-0.261799387799, 0.0, 0.0],
-    false,
-    null
+    false
 );
 
-var trapezium = new Bone("bones/_r_trpzm.asc", null, null, dist_trpzm, true, auxdrrud, []);
+var trapezium = new Bone("bones/_r_trpzm.asc", dist_trpzm, true, auxdrrud, []);
 
 var dist_trpzd = new Joint(
     [-0.0015, -0.0044, 0.0134],
     [0.349065850399, -0.174532925199, 0.0],
-    false,
-    null
+    false
 );
 
-var trapezoid = new Bone("bones/_r_trpzd.asc", null, null, dist_trpzd, true, auxdrrud, []);
+var trapezoid = new Bone("bones/_r_trpzd.asc", dist_trpzd, true, auxdrrud, []);
 
 var dist_cap = new Joint(
     [-0.0155, -0.0018, -0.0055],
     [0.174532925199, 0.0, 0.0],
-    false,
-    null
+    false
 );
 
-var capitate = new Bone("bones/_r_cap.asc", null, null, dist_cap, true, auxdrrud, []);
+var capitate = new Bone("bones/_r_cap.asc", dist_cap, true, auxdrrud, []);
 
 var prox_pis = new Joint(
     [0.005, -0.015, -0.0201],
     [0, 0, 0],
-    false,
-    null
+    false
 );
 
-var pisiform = new Bone("bones/_r_pis.asc", null, null, prox_pis, true, auxprrud, []);
+var pisiform = new Bone("bones/_r_pis.asc", prox_pis, true, auxprrud, []);
 
 var prox_triq = new Joint(
     [-0.0055, -0.01, -0.021],
     [0, 0, 0],
-    false,
-    null
+    false
 );
 
-var triquetrum = new Bone("bones/_r_triq.asc", null, null, prox_pis, true, auxprrud, []);
+var triquetrum = new Bone("bones/_r_triq.asc",  prox_pis, true, auxprrud, []);
 
 var dist_ham = new Joint(
     [-0.006, -0.005, -0.0175],
     [-0.209439510239, 0.0, 0.0],
-    false,
-    null
+    false
 );
 
-var hamate = new Bone("bones/_r_ham.asc", null, null, dist_ham, true, auxdrrud, []);
+var hamate = new Bone("bones/_r_ham.asc", dist_ham, true, auxdrrud, []);
 
 var auxCMC = new Joint(
     [0, 0, 0],
     [0, 0, 0],
-    false,
-    null
+    false
 );
 
-var carp_metacarp = new Bone(null, null, null, auxCMC, false, auxdrrud, []);
+var carp_metacarp = new Bone(null, auxCMC, false, auxdrrud, []);
 
 var CMC2 = new Joint(
     [-0.0047, -0.0095, -0.005],
     [0.174532925199, 0.261799387799, 0.0],
-    false,
-    null
+    false
 );
 
-var metacarpal2 = new Bone("bones/_fingers1.asc", null, null, CMC2, true,  carp_metacarp, []);
+var metacarpal2 = new Bone("bones/_fingers1.asc", CMC2, true,  carp_metacarp, []);
 
+var MCP2_lateral = new Motion(
+    [1, 0, 0],
+    -0.349065850399,
+    0.349065850399,
+    0
+);
 
+var MCP2_flex = new Motion(
+    [0, 0, 1],
+    -0.174532925199,
+    1.570796326795,
+    0
+)
 
-function initSlider(bone, num) {
+var MCP2 = new Joint(
+    [-0.0024, -0.0685, 0.045],
+    [0 ,0, 0],
+    true
+);
+
+MCP2.addMotion(MCP2_lateral);
+MCP2.addMotion(MCP2_flex);
+
+var Iphalanx1 = new Bone("bones/_fingers2.asc", MCP2, true, metacarpal2, []);
+
+var PIP_flex = new Motion(
+    [0, 0, 1],
+    -0.0872664626,
+    1.570796326795,
+    0
+);
+
+var PIP = new Joint(
+    [0.0005, -0.04826, 0.00799],
+    [0, 0, 0],
+    true
+);
+
+PIP.addMotion(PIP_flex);
+
+var Iphalanx2 = new Bone("bones/_fingers3.asc", PIP, true, Iphalanx1, []);
+
+var DIP_flex = new Motion(
+    [0, 0, 1],
+    -0.0872664626,
+    1.570796326795,
+    0
+);
+
+var DIP = new Joint(
+    [0.00193, -0.03277, 0.00472],
+    [0, 0, 0],
+    true
+);
+DIP.addMotion(DIP_flex);
+
+var Iphalanx3 = new Bone("bones/_fingers4.asc", DIP, true, Iphalanx2, []);
+
+function initSlider(bone, num, numOM) {
     var div = document.createElement('div');
-    div.id = "Moving"+num.toString();
+    div.id = "Motion_of_Bone"+num.toString()+"_num" + numOM.toString();
 
     document.body.appendChild(div);
 
     $("#" + div.id).slider({
-        min: bone.Joint.Motion.min,
-        max: bone.Joint.Motion.max,
+        min: bone.Joint.Motions[numOM].min,
+        max: bone.Joint.Motions[numOM].max,
         step: 0.001,
         slide: function( event, ui ) {
-            bone.Joint.Motion.cur_value = ui.value;
+            bone.Joint.Motions[numOM].cur_value = ui.value;
         }
     }).slider("float");
 
