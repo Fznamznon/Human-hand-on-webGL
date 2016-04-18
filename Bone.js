04e8c55cf7b521d6b0ac5885b2e2addca76e2221
Mary <fznamznon23@gmail.com>
2016-04-18 21:16:51 +0300

    add complex_motion

--- Bone.js
+++ Bone.js
@@ -1,4 +1,5 @@
 var Bones = [];
+var Compex_Motions = [];
 
 function Bone(filename, Joint, Visible, parent, childs) {
     this.filename = filename;
@@ -24,14 +25,31 @@ function Joint(location, orientation, movable) {
     }
 }
 
-function Motion(axis, min, max, cur_value) {
+function Motion(axis, min, max, cur_value, name) {
     this.axis = axis;
     this.min = min;
     this.max = max;
     this.cur_value = cur_value;
+    this.name = name;
 }
 
+function Complex_Motion(name) {
+    this.motions = [];
+    this.T = 0;
 
+    this.linAppr = function(x1, y1, x2, y2, t)
+    {
+        return y1 + (y2 - y1)/(x2 - x1)*(t - x1);
+    }
+    
+    this.add = function (motion) {
+        this.motions.push(motion);
+    }
+    this.name = name;
+    Compex_Motions.push(this);
+}
+
+var point_finger_flexion = new Complex_Motion("point_finger_flexion");
 
 
 var humerus = new Bone("bones/humerus.asc", null, true, null, [] );
@@ -39,7 +57,8 @@ var humerus = new Bone("bones/humerus.asc", null, true, null, [] );
 var elbow_flexion = new Motion([0.226046935824, 0.191039945763, 0.955199728815],
     0.0,
     2.268928027593,
-    0.0
+    0.0,
+    "elbow_flexion"
 );
 
 
@@ -57,7 +76,8 @@ var pro_sup = new Motion(
     [0.056398022307, 0.983577389037, -0.171449067814],
     -1.570796326795,
     1.570796326795,
-    0.0
+    0.0,
+    "pro_sup"
 );
 
 
@@ -85,7 +105,8 @@ var flexion = new Motion(
     [0, 0, 1],
     -1.221730476396,
     1.221730476396,
-    0.0
+    0.0,
+    "flexion"
 );
 
 var rotaxis_auxprfem = new Joint(
@@ -102,7 +123,8 @@ var deviation = new Motion(
     [1, 0, 0],
     -0.436332312999,
     0.610865238198,
-    0.0
+    0.0,
+    "deviation"
 );
 
 var auxprfem_auxprrud = new Joint(
@@ -216,14 +238,16 @@ var MCP2_lateral = new Motion(
     [1, 0, 0],
     -0.349065850399,
     0.349065850399,
-    0
+    0,
+    "MCP2_lateral"
 );
 
 var MCP2_flex = new Motion(
     [0, 0, 1],
     -0.174532925199,
     1.570796326795,
-    0
+    0,
+    "MCP2_flex"
 )
 
 var MCP2 = new Joint(
@@ -234,6 +258,10 @@ var MCP2 = new Joint(
 
 MCP2.addMotion(MCP2_lateral);
 MCP2.addMotion(MCP2_flex);
+point_finger_flexion.add(MCP2_flex);
+//point_finger_flexion.add(MCP2_lateral);
+
+
 
 var Iphalanx1 = new Bone("bones/_fingers2.asc", MCP2, true, metacarpal2, []);
 
@@ -241,8 +269,10 @@ var PIP_flex = new Motion(
     [0, 0, 1],
     -0.0872664626,
     1.570796326795,
-    0
+    0,
+    "PIP_flex"
 );
+point_finger_flexion.add(PIP_flex);
 
 var PIP = new Joint(
     [0.0005, -0.04826, 0.00799],
@@ -258,7 +288,8 @@ var DIP_flex = new Motion(
     [0, 0, 1],
     -0.0872664626,
     1.570796326795,
-    0
+    0,
+    "DIP_flex"
 );
 
 var DIP = new Joint(
@@ -267,6 +298,8 @@ var DIP = new Joint(
     true
 );
 DIP.addMotion(DIP_flex);
+point_finger_flexion.add(DIP_flex);
+
 
 var Iphalanx3 = new Bone("bones/_fingers4.asc", DIP, true, Iphalanx2, []);
 
@@ -278,11 +311,15 @@ var CMC3 = new Joint(
 
 var metacarpal3 = new Bone("bones/_fingers8.asc", CMC3, true, carp_metacarp, []);
 
+
+
 var MCP3 = new Joint(
     [0.0, -0.003, 0.0],
     [0, 0, 0],
     false
 );
+
+
 
 var midfinger1 = new Bone("bones/_fingers5.asc", MCP3, true, metacarpal3, []);
 var midfinger2 = new Bone("bones/_fingers6.asc", MCP3, true, metacarpal3, []);
@@ -329,14 +366,16 @@ var thumb_abd = new Motion(
     [1, 0, 0],
     -0.785398163397,
     0.261799387799,
-    0
+    0,
+    "thumb_abd"
 );
 
 var thumb_flex = new Motion(
     [0.056398022307, 0.983577389037, -0.171449067814],
     -0.785398163397,
     1.570796326795,
-    0
+    0,
+    "thumb_flex"
 );
 
 var CMC1 = new Joint(
@@ -354,27 +393,86 @@ var CMC1_1 = new Joint(
     [0, 0, 0],
     [0, 0, 0],
     false
-)
+);
 
 var thumb_1 = new Bone("bones/_fingers18.asc", CMC1_1, true, thumb, []);
 var thumb_2 = new Bone("bones/_fingers19.asc", CMC1_1, true, thumb, []);
 
 
 
-
 function initSlider(bone, num, numOM) {
     var div = document.createElement('div');
-    div.id = "Motion_of_Bone"+num.toString()+"_num" + numOM.toString();
+    div.id = bone.Joint.Motions[numOM].name;
 
-    document.body.appendChild(div);
+    var header = document.createElement("p");
+    header.innerHTML = bone.Joint.Motions[numOM].name;
+    var c = document.getElementById("SmallMotions");
+    c.appendChild(header);
+    c.appendChild(div);
+
 
     $("#" + div.id).slider({
         min: bone.Joint.Motions[numOM].min,
         max: bone.Joint.Motions[numOM].max,
         step: 0.001,
+        value: bone.Joint.Motions[numOM].cur_value,
         slide: function( event, ui ) {
             bone.Joint.Motions[numOM].cur_value = ui.value;
         }
     }).slider("float");
 
+}
+
+function initSliderForCompexMotion(Mot) {
+    
+    var div = document.createElement('div');
+    div.id = Mot.name;
+
+    var c = document.getElementById("ComplexMotions");
+    var header = document.createElement("p");
+    header.innerHTML = Mot.name;
+
+    c.appendChild(header);
+    c.appendChild(div);
+
+    $("#" + div.id).slider({
+        min: 0,
+        max: 1,
+        step: 0.001,
+        slide: function( event, ui ) {
+            var x1 = [];
+            var y1 = [];
+            var x2 = [];
+            var y2 = [];
+            var i;
+            if (Mot.T < ui.value)
+            {
+                i = 0;
+                for (; i < Mot.motions.length; ++i){
+                    x1.push(Mot.T);
+                    x2.push(1);
+                    y1.push(Mot.motions[i].cur_value);
+                    y2.push(Mot.motions[i].max);
+                }
+            }
+            else
+            {
+                i = 0;
+                for (; i < Mot.motions.length; ++i){
+                    x1.push(0);
+                    x2.push(Mot.T);
+                    y1.push(Mot.motions[i].min);
+                    y2.push(Mot.motions[i].cur_value);
+                }
+
+            }
+            i = 0;
+            for (; i < Mot.motions.length; ++i){
+                Mot.motions[i].cur_value = Mot.linAppr(x1[i], y1[i], x2[i], y2[i], ui.value);
+                $("#" + Mot.motions[i].name).slider("value", Mot.motions[i].cur_value);
+            }
+            Mot.T = ui.value;
+        }
+    }).slider("float");
+    
 }
